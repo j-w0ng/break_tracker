2deab9fb50d1db9d99da03614ac2785e0ebcf5a5
j-w0ng <45083230+j-w0ng@users.noreply.github.com>
2019-02-17 14:07:45 -0800

    additional page content modifications

--- content.js
+++ content.js
@@ -10,13 +10,43 @@ chrome.runtime.onMessage.addListener(
     if (msg.message === "breakEnd") {
         let paragraph = document.getElementsByTagName("p");
         for (elt of paragraph) {
-            elt.innerHTML = "SHOULD YOU BE HERE STILL?"
+            elt.innerHTML = "SHOULD YOU STILL BE HERE?"
+            elt.style["color"] = "#FF69B4";
+        }
+        let header2 = document.getElementsByTagName("h2");
+        for (elt of header2) {
+            elt.innerHTML = "SHOULD YOU STILL BE HERE?"
+            elt.style["color"] = "#FF69B4";
+        }
+        let header3 = document.getElementsByTagName("h3");
+        for (elt of header3) {
+            elt.innerHTML = "SHOULD YOU STILL BE HERE?"
+            elt.style["color"] = "#FF69B4";
+        }
+        let header4 = document.getElementsByTagName("h4");
+        for (elt of header4) {
+            elt.innerHTML = "SHOULD YOU STILL BE HERE?"
             elt.style["color"] = "#FF69B4";
         }
     }
     if (msg.message === "turnOn") {
         let paragraph = document.getElementsByTagName("p");
         for (elt of paragraph) {
+            elt.innerHTML = "You Should Be Studying"
+            elt.style["color"] = "#FF69B4";
+        }
+        let header2 = document.getElementsByTagName("h2");
+        for (elt of header2) {
+            elt.innerHTML = "You Should Be Studying"
+            elt.style["color"] = "#FF69B4";
+        }
+        let header3 = document.getElementsByTagName("h3");
+        for (elt of header3) {
+            elt.innerHTML = "You Should Be Studying"
+            elt.style["color"] = "#FF69B4";
+        }
+        let header4 = document.getElementsByTagName("h4");
+        for (elt of header4) {
             elt.innerHTML = "You Should Be Studying"
             elt.style["color"] = "#FF69B4";
         }
